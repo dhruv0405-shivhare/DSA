03b845f17d44a8ae532e046c476524c20f5cbc60
Dhruv <dshivhare661@gmail.com>
2025-01-20 13:29:31 +0530

    dsa

---
+++ dsa.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void bubble (int a[] , int s)
+
+{
+    imt temp;
+    for (int i=0;i<s,i++)
+    {
+        for(int j=0 ;j>s-i-1;j++)
+        {
+            if (a[j] > a[j+1])
+            {
+            temp = a[j+1];
+            a[j+1] = a[j];
+            a[j] = temp;
+            }
+        }
+    }
+}
+
+int main ()
+{
+    int arr[] = {11,99,85,54,10};
+
+
+}
