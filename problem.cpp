ecdfa593beb22d9773f71f46c40b4cb9cfdd7dac
Dhruv <dshivhare661@gmail.com>
2025-02-22 13:26:55 +0530

    template

--- problem.cpp
+++ problem.cpp
@@ -15,3 +15,17 @@
 // template is a generalize format of any program that may be fuction or class  
 // two types of template
 // 1. function template, 2. class template
+
+
+#include<iostream>
+using namespace std;
+template<typename t1>
+// content place holder ---> < >
+t1 sum (t1 a, t2 b)
+{
+    return a + b;
+}
+int main()
+{
+    cout<<sum(2.5,3.6);
+}
