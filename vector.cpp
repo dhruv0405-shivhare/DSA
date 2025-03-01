12ffd561deb4d796c944bdd501843e37c9c16e31
Dhruv <dshivhare661@gmail.com>
2025-03-01 13:15:23 +0530

    vector

--- vector.cpp
+++ vector.cpp
@@ -1,25 +1,41 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
 #include<vector>
+using namespace std;
 int main()
 {
-    vector<int>v{3,2,5,1};
-    v[1]=90;
-    vector<string>v2{"sun" , "mon" , "tues"};
-    for (auto p:v)
+    vector<int>v{2,5,3,7,8,9};
+    v[1] = 90;
+    vector<string>v2{"sun" , "moon" , "earth"};
+    for(auto k:v2)
     {
-        cout<<p<<"\t";
+        cout << k << "\t";
     }
     vector<int>v3(5);
     cout<<"\n";
     for(auto k:v3)
     {
-        cout<<k<<"\t";
+        cout << k << "\t";
     }
+    cout << "\n";
     vector<int>v4(5,10);
-    v4[3]=2000;
+    v4[3] = 2000;
     for(auto k:v4)
     {
-        cout<<k<<"\t"
+        cout << k << "\t";
     }
+    vector<int>vec;
+    int n ,a ;
+    cout << "enter the size of vector \n";
+    cin>>n;
+    for(int i = 1 ; i <= n ; i++)
+    {
+        cout << "enter value \t";
+        cin >> a;
+        vec.push_back(a);
+    }
+    cout << "data of vector is \n";
+    for(auto p:vec)
+    {
+        cout << p << "\t";
+    }
 }
